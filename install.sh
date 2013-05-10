959a16cd09c0251db50fec9c89dd69cb1b5b6e43
rcmdnk <Michiru.Kaneda@gmail.com>
2013-05-10 16:07:52 +0200

    add gtaskslist.py

---
+++ install.sh
@@ -0,0 +1,128 @@
+#!/bin/bash
+exclude=('.' '..' 'README.md' 'install.sh')
+instdir="$HOME/usr/bin"
+
+backup="bak"
+overwrite=1
+dryrun=0
+newlink=()
+exist=()
+curdir=`pwd -P`
+# help
+HELP="Usage: $0 [-nd] [-b <backup file postfix>] [-e <exclude file>] [-i <install dir>]
+
+Make links of scripts (default:in $instdir)
+
+Arguments:
+      -b  Set backup postfix (default: make *.bak file)
+          Set \"\" if backups are not necessary
+      -e  Set additional exclude file (default: ${exclude[@]})
+      -i  Set install directory (default: $instdir)
+      -n  Don't overwrite if file is already exist
+      -d  Dry run, don't install anything
+      -h  Print Help (this message) and exit
+"
+while getopts b:e:i:ndh OPT;do
+  case $OPT in
+    "b" ) backup=$OPTARG ;;
+    "e" ) exclude=(${exclude[@]} "$OPTARG") ;;
+    "i" ) instdir="$OPTARG" ;;
+    "n" ) overwrite=0 ;;
+    "d" ) dryrun=1 ;;
+    "h" ) echo "$HELP" 1>&2; exit ;;
+    * ) echo "$HELP" 1>&2; exit ;;
+  esac
+done
+
+if [[ "$OSTYPE" =~ "cygwin" ]];then
+  # ln wrapper{{{
+  function ln {
+    opt="/H"
+    if [ "$1" = "-s" ];then
+      opt=""
+      shift
+    fi
+    target="$1"
+    if [ -d "$target" ];then
+      opt="/D $opt"
+    fi
+    if [ $# -eq 2 ];then
+      link="$2"
+    elif [ $# -eq 1 ];then
+      link=`basename "$target"`
+    else
+      echo "usage: ln [-s] <target> [<link>]"
+      echo "       -s for symbolic link, otherwise make hard link"
+      return
+    fi
+    t_winpath=$(cygpath -w -a "$target")
+    t_link=$(cygpath -w -a "$link")
+    echo "cmd /c mklink $opt $t_link $t_winpath"
+    cmd /c mklink $opt "$t_link" "$t_winpath"
+  }
+# }}}
+fi
+
+echo "**********************************************"
+echo "Install X.py to $instdir/X"
+echo "**********************************************"
+echo
+if [ $dryrun -ne 1 ];then
+  mkdir -p $instdir
+else
+  echo "*** This is dry run, not install anything ***"
+fi
+for f in *.py;do
+  for e in ${exclude[@]};do
+    flag=0
+    if [ "$f" = "$e" ];then
+      flag=1
+      break
+    fi
+  done
+  if [ $flag = 1 ];then
+    continue
+  fi
+  name=${f%.py}
+  install=1
+  if [ $dryrun -eq 1 ];then
+    install=0
+  fi
+  if [ "`ls "$instdir/$name" 2>/dev/null`" != "" ];then
+    exist=(${exist[@]} "$name")
+    if [ $dryrun -eq 1 ];then
+      echo -n ""
+    elif [ $overwrite -eq 0 ];then
+      install=0
+    elif [ "$backup" != "" ];then
+      mv "$instdir/$name" "$instdir/${name}.$backup"
+    else
+      rm "$instdir/$name"
+    fi
+  else
+    newlink=(${newlink[@]} "$name")
+  fi
+  if [ $install -eq 1 ];then
+    ln -s "$curdir/$f" "$instdir/$name"
+  fi
+done
+echo ""
+if [ $dryrun -eq 1 ];then
+  echo "Following files don't exist:"
+else
+  echo "Following files were newly installed:"
+fi
+echo "  ${newlink[@]}"
+echo
+echo -n "Following files existed"
+if [ $dryrun -eq 1 ];then
+  echo "Following files exist:"
+elif [ $overwrite -eq 0 ];then
+  echo "Following files exist, remained as is:"
+elif [ "$backup" != "" ];then
+  echo "Following files existed, backups (*.$backup) were made:"
+else
+  echo "Following files existed, replaced old one:"
+fi
+echo "  ${exist[@]}"
+echo
